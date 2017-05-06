8a28f9c5e090eb663de35cb9c2ab1d7fa9b75635
Ed Phillips <ed@paperuncle.com>
2017-05-06 15:59:32 +0100

    default header, footer

--- App/Components/Footer.js
+++ App/Components/Footer.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 class Footer extends Component {
   render() {
     return (
-      <View>
-        <Text>THIS IS THE FOOTER</Text>
+      <View style={styles.footer}>
+        <Text style={{color: 'white'}}>THIS IS THE FOOTER</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  footer: {
+    height: 50,
+    backgroundColor: '#222',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
 
 export default Footer;
--- App/Components/Header.js
+++ App/Components/Header.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 class Header extends Component {
   render() {
     return (
-      <View>
-        <Text>THIS IS THE HEADER</Text>
+      <View style={styles.header}>
+        <Text style={{color: 'white'}}>THIS IS THE HEADER</Text>
         {this.props.children}
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  header: {
+    height: 50,
+    backgroundColor: '#222',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
 
 export default Header;
