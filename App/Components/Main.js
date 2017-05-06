d5171ec96751e2a8d9e355908c60be7d16934d8a
Ed Phillips <ed@paperuncle.com>
2017-05-06 16:00:24 +0100

    query API

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -1,44 +1,97 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import axios from 'axios';
+
 import Header from './Header';
+import Listing from './Listing';
 import Footer from './Footer';
 
 class Main extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      long: null,
       lat: null,
-      error: null
+      long: null,
+      error: null,
+      establishments: []
     }
+
+    this.bringPlaces = this.bringPlaces.bind(this);
   }
   
   componentDidMount() {
+
     navigator.geolocation.getCurrentPosition(
       position => {
-        console.log('goelocate success', this.state.long);
-        this.setState({
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          error: null,
-        });
+        // setTimeout(() => {
+          this.setState({
+            lat: position.coords.latitude,
+            long: position.coords.longitude,
+            error: null,
+          },
+          // once lat and long states have been set, use the callback
+          // of setState to call the axios search which rely on theese
+          // coordinates
+          this.bringPlaces
+        );
+        // }, 10000);
+        
       },
       error => this.setState({ error: error.message }),
-      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+      { enableHighAccuracy: false, timeout: 5000, maximumAge: 0 },
     );
   }
   
 
+  bringPlaces() {
+    console.log('bringPlaces() called')
+    console.log('lat', this.state.lat)
+    console.log('long', this.state.long)
+
+    const getPlaces = axios.create({
+      baseURL: 'http://api.ratings.food.gov.uk/Establishments/',
+      timeout: 10000,
+      headers: {'x-api-version': '2', 'accept': 'application/json', 'content-type': 'application/json'},
+      params: {
+        latitude: this.state.lat,
+        longitude: this.state.long,
+        maxDistanceLimit: 1,
+        sortOptionKey: 'distance',
+        businessTypeId: 1,
+        pageSize: 20
+      },
+    });
+
+    getPlaces()
+      .then( response => {
+        if (response.status === 200) {
+
+          ///////////////
+          // lets sort results by the Distance key
+          ///////////////
+          
+          this.setState({
+            establishments: response.data.establishments
+          })
+        }
+      })
+      .catch( error => {
+        console.log('error', error);
+      })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Header style={styles.header}>
-          <Text>Latitude: {this.props.lat}</Text>
-          <Text>Longitude: {this.props.long}</Text>
-          {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+        <Header>
+          
+          {console.log('establishments', this.state.establishments)}
+          <Text style={{color: 'white'}}>Latitude: {this.state.lat}</Text>
+          <Text style={{color: 'white'}}>Longitude: {this.state.long}</Text>
         </Header>
         <View style={styles.content}>
-          <Text>MAIN VIEW</Text>
+          {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+          <Listing long={this.state.long} lat={this.state.lat} establishments={this.state.establishments} />
         </View>
         <Footer />
       </View>
@@ -49,15 +102,18 @@ class Main extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    
     backgroundColor: 'yellow',
-    marginTop: 22
-  },
-  header: {
-    flex: 1,
-    backgroundColor: 'blue'
+    paddingTop: 22,
   },
   content: {
-    flex: 1
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
   }
 })
 
