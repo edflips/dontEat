47b0b4e6ba9c9138c310e5ed74c48eccbf835d77
Ed Phillips <ed@paperuncle.com>
2017-05-10 15:03:40 +0100

    add separator

---
+++ App/Components/ListSeparator.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const ListSeparator = () =>
+  <View style={{
+    height: 1,
+    backgroundColor: '#ccc',
+    width: '100%'
+  }}></View>
+
+  export default ListSeparator
