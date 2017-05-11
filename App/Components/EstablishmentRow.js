c751a228349911818bbe9b50c80c4e64941504f5
Ed Phillips <ed@paperuncle.com>
2017-05-11 18:58:47 +0100

    reformat address in list and change style

--- App/Components/EstablishmentRow.js
+++ App/Components/EstablishmentRow.js
@@ -8,8 +8,8 @@ class EstablishmentRow extends Component {
     let address = []
     this.props.address1 ? address.push(this.props.address1) : null
     this.props.address2 ? address.push(this.props.address2) : null
-    this.props.postcode ? address.push(this.props.postcode) : null
-    address = address.join('\n')
+    // this.props.postcode ? address.push(this.props.postcode) : null
+    address = address.join(', ')
 
     // check if the score is a number
     const score = (num) => !isNaN(parseFloat(num)) ? num : 'N'
@@ -21,8 +21,9 @@ class EstablishmentRow extends Component {
             <Text style={styles.scoreText}>{score(this.props.score)}</Text>
           </View>
           <View style={styles.nameWrap}>
-            <Text style={{fontSize: 20}}>{this.props.name}</Text>
-            <Text>{address}</Text>
+            <Text style={{fontSize: 20, color: 'white'}}>{this.props.name}</Text>
+            <Text style={{color: 'white'}}>{address}</Text>
+            <Text style={{color: 'white'}}>{this.props.postcode}</Text>
           </View>
         </View>
       </TouchableHighlight>
@@ -46,19 +47,41 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
 
     padding: 8,
-    backgroundColor: 'white',
-    height: 100
+    backgroundColor: '#53777A',
+    height: 75
   },
 
   scoreWrap: {
+    // flexDirection: 'column',
+    // justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: 'white',
     width: 50,
+    height: 50,
+    marginTop: 5,
+    marginRight: 13,
+    marginLeft: 5,
+    borderRadius: 100
+    
   },
   scoreText: {
-    fontSize: 50,
+    color: '#53777A',
+    fontSize: 42,
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    // backgroundColor: 'green',
+    // width: 50,
+    // height: 50,
+    backgroundColor: 'transparent',
+    // padding: 10
+    
+    
+    
   },
 
   nameWrap: {
-    // flex: 1,
+    flex: 1,
   }
 })
 
